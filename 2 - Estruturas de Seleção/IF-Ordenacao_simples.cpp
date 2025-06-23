cb874f4f529c38eb49defb6e1b989280ca58f938
RodrigoCAndrade <198579161+RodrigoCAndrade@users.noreply.github.com>
2025-06-22 23:14:36 -0300

    ☀️ Added IF-Ordenacao_simples.cpp

---
+++ 2 - Estruturas de Seleção/IF-Ordenacao_simples.cpp
@@ -0,0 +1,30 @@
+/**
+ * @file: IF-Ordenacao_simples.cpp
+ * @author: Rodrigo Andrade
+ * @date: 22 Feb 2025
+ * @version: 2.0
+ * @license: MIT
+ * @language: C++
+ * @github: https://github.com/RodrigoCAndrade/C02
+ */
+
+#include <iostream>
+
+int main() {
+  // Lê e armazena n1, n2 e n3.
+  int n1, n2, n3;
+  std::cin >> n1 >> n2 >> n3;
+
+  // Ordena n1, n2, n3 do maior para o menor usando swap e ifs.
+  if (n1 > n2) std::swap(n1, n2);
+  if (n1 > n3) std::swap(n1, n3);
+  if (n2 > n3) std::swap(n2, n3);
+
+  // Exibe o resultado.
+  std::cout << std::endl
+            << n1 << std::endl
+            << n2 << std::endl
+            << n3 << std::endl;
+
+  return 0;
+}
